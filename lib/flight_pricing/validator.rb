0165c45b069987644a8c443d2f77fe665c3236af
Harold Perez <haroldjose2903@gmail.com>
2024-04-26 03:06:47 -0300

    [add_flight_confirmation] Add update flight pricing for flight offers

--- lib/flight_pricing/validator.rb
+++ lib/flight_pricing/validator.rb
@@ -8,13 +8,29 @@ module FlightPricing
       return unless inside_ticketing_period?
 
       response = amadeus_client.post_flight_pricing(build_request_params)
-
       parsed_response = ResponseParser.parse(response, flight_offers)
+
+      flight_offers.each do |flight_offer|
+        flight_offer.update(parsed_response)
+      end
     end
 
     private
 
     attr_reader :flight_offers
+
+    def update_flight_offers(response)
+      flight_offers.each do |flight_offer|
+        flight_offer.price.update(response[:price])
+        flight_offer.traveler_pricings.each do |traveler_pricing|
+          response[:traveler_pricings_attributes].each do |response_traveler_pricing|
+            if traveler_pricing.traveler_internal_id == response_traveler_pricing[:traveler_internal_id]
+              traveler_pricing.price.update(response_traveler_pricing[:price_attributes])
+            end
+          end
+        end
+      end
+    end
 
     def inside_ticketing_period?
       flight_offer.first.last_ticketing_datetime > Time.now # TODO: one single flight offer?
